6bf469191caf7f0251e1e2c9d072d040ee36f0c7
Robert John Cabrera <robertjohncabrera927@gmail.com>
2026-03-03 13:51:02 +0800

    Add Home UI

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -1,5 +1,17 @@
 function Index() {
-  return <div>Home</div>;
+  return (
+    <div className="shadow bg-white rounded max-w-4xl mx-auto mt-5 p-5 text-black">
+      <h1 class="text-3xl uppercase text-center">React Quiz App</h1>
+      <hr className="mt-4 mb-4 text-gray-300" />
+      <button className="bg-violet-500 text-white w-full rounded-full py-3 px-2 mb-3 hover:bg-violet-600 cursor-pointer">
+        Start Quiz
+      </button>
+
+      <button className="bg-violet-500 text-white w-full rounded-full py-3 px-2 hover:bg-violet-600 cursor-pointer">
+        View Leaderboard
+      </button>
+    </div>
+  );
 }
 
 export default Index;
